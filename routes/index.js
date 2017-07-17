0f6b01508dcf03e5058441051757bf2b6e65915d
Muthuramakrishnan <mviswanathan@google.com>
2017-07-16 20:08:16 -0700

    Update files

--- routes/index.js
+++ routes/index.js
@@ -43,7 +43,7 @@ function checkUser(req, res, next) {
   var xml = req.body;
   console.log(JSON.stringify(xml));
   var username = xml['SOAP-ENV:Envelope']['SOAP-ENV:Body'][0]['ns:callTXLife'][0]['callTXLifeRequest'][0]['TXLife'][0]['UserAuthRequest'][0]['UserLoginName'][0];
-
+  req.username = username;
   console.log(username);
 
   queryDb('SELECT * FROM POC_TBL_USERS AS usr WHERE USERNAME=\'' + username + '\'', function (error, results, fields) {
@@ -77,12 +77,67 @@ function checkPolicy(req, res, next) {
   });
 }
 
+//UPDATE `POC_TBL_USERS` SET  `PHOTO_FILE_NAME` = "pho1.jpg" WHERE `USERNAME` =  "jeffery"
+
+function updateUserPic(req, res, next){
+  console.log("upload_query:" + 'UPDATE POC_TBL_USERS SET PHOTO_FILE_NAME = \'' + req.fname + '\' WHERE USERNAME = \'' + req.body.username + '\'');
+  queryDb('UPDATE POC_TBL_USERS SET PHOTO_FILE_NAME = \'' + req.fname + '\' WHERE USERNAME = \'' + req.body.username + '\'', function (error, results, fields) {
+    if (error) {
+      console.log(error);
+      render('phtfail', req, res, next);
+    } else {
+      console.log("upload_scss");
+      updateUserAddress(req, res, next);
+    }
+  });
+}
+
+function updateUserAddress(req, res, next){
+  var addr_line1 = req.body.ad1;
+  var addr_line2 = req.body.ad2;
+  var addr_line3 = req.body.ad3;
+  var city = req.body.city;
+  var province = req.body.province;
+  var zip = req.body.zip;
+  var country = req.body.country;
+  getUserID(req.body.username, function(userid){
+    console.log("query+ " + 'UPDATE POC_TBL_CIF_CLIENT_ADDRESSES SET ADDRESS_LINE_1 = \'' + addr_line1 + '\',ADDRESS_LINE_2 = \'' + addr_line2 + '\',ADDRESS_LINE_3 = \'' + addr_line3 + '\',CITY_NAME = \'' + city + '\',PROVINCE_NAME = \'' + province + '\',ZIP_CODE = \'' + zip + '\',COUNTRY_NAME = \'' + country + '\'' + 'WHERE ADDRESS_TYPE = \'HOME\' AND CIF_CLIENT_NO = \'' + userid + '\'');
+    queryDb('UPDATE POC_TBL_CIF_CLIENT_ADDRESSES SET ADDRESS_LINE_1 = \'' + addr_line1 + '\',ADDRESS_LINE_2 = \'' + addr_line2 + '\',ADDRESS_LINE_3 = \'' + addr_line3 + '\',CITY_NAME = \'' + city + '\',PROVINCE_NAME = \'' + province + '\',ZIP_CODE = \'' + zip + '\',COUNTRY_NAME = \'' + country + '\'' + 'WHERE ADDRESS_TYPE = \'HOME\' AND CIF_CLIENT_NO = \'' + userid + '\'' , function (error, results, fields) {
+      if (error) {
+        console.log(error);
+        render('dtfail', req, res, next);
+      } else {
+        console.log("upload_scss");
+        console.log(results);
+        render('phtscss', req, res, next);
+      }
+    });
+  });
+}
+
+function getUserID(username, callback) {
+  queryDb('SELECT * FROM POC_TBL_USERS WHERE USERNAME = \'' + username + '\'', function (error, results, fields) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.stringify(results[0]));
+      var b = JSON.parse(JSON.stringify(results[0]));
+      console.log(b);
+      callback(b.CIF_CLIENT_NO);
+    }
+  });
+}
+
 function render(type, req, res, next) {
   var file = '/fail.xml';
   if(type == 'policy-err') {
     file = '/policyerr.xml';
   } else if(type == 'scss') {
     file = '/scss.xml';
+  } else if(type == 'phtscss') {
+    file = '/phtscss.xml';
+  } else if(type == 'phtfail') {
+    file = '/phtfail.xml';
   }
 
   fs.readFile(filePath + file, {encoding: 'utf-8'}, function(err,data){
@@ -119,10 +174,10 @@ router.post('/TXLifeUpload', function(req, res, next) {
           c.end();
           if (err) {
             console.log(err);
-            res.status(500).send({ error: "Upload failed" });
+            render('phtfail', req, res, next);
           } else {
             console.log("File transferred successfully!");
-            res.status(200).send({ resp: "Upload success" });
+            updateUserPic(req, res, next);
           }
         });
       });
